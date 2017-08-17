51c0f9e43b43cb3cba72e24038774f1889b38457
William <william.gaiteiro@gmail.com>
2017-08-16 21:37:49 -0300

    Adicionado metodos delete, getById e getList para agricultor

--- controllers/agricultor.js
+++ controllers/agricultor.js
@@ -1,23 +1,99 @@
 const _ = require('lodash');
 
 const { Agricultor } = require('../models/agricultor');
+const { ObjectID } = require('mongodb')
 
-var create = (req, res) => {
-
-	console.log(req.body);
-
-    var body = _.pick(req.body, ['login', 'senha', 'nomeCompleto', 'cpf', 'email', 'telefone', 'whattsapp', 'bloqueado', 'visitas', 'sexo', 'imagemPerfil', 'dataCriacaoConta', 'dataNascrimento', 'hashConfirmacao']);
-
-	console.log(JSON.stringify(body, undefined, 4));
+const create = (req, res) => {
+	var body = _.pick(req.body, ['login', 'senha', 'nomeCompleto', 
+	'cpf', 'email', 'telefone', 'whattsapp', 'bloqueado', 'visitas', 
+	'sexo', 'imagemPerfil', 'dataCriacaoConta', 'dataNascrimento', 
+	'hashConfirmacao']);
 
     var agricultor = new Agricultor(body)
 
-	agricultor.save().then((doc) => {
-		res.send(doc)
-	}, (e) => {
-		res.status(400).send(e)
-	})
-}
+    agricultor.save().then((doc) => {
+        return res.send(doc)
+    }, (e) => {
+        return res.status(400).send(e)
+    })
+};
 
+const remove = (req, res) => {
+	var id = req.params.id
 
-module.exports = { create }
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	Agricultor.findByIdAndRemove(id).then((agricultor) => {
+		if (!agricultor) {
+			return res.status(404).send()
+		}
+		return res.send({agricultor})
+	}).catch((e) => res.status(400).send())
+};
+
+const update = (req, res) => {
+	var body = _.pick(req.body, ['_id', 'login', 'senha', 'nomeCompleto', 
+	'cpf', 'email', 'telefone', 'whattsapp', 'bloqueado', 'visitas', 
+	'sexo', 'imagemPerfil', 'dataCriacaoConta', 'dataNascrimento', 
+	'hashConfirmacao']);
+
+	console.log(body);
+
+	var agricultor = new Agricultor(body)
+
+	id = agricultor._id;
+	
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	if (_.isBoolean(agricultor.completed) && agricultor.completed) {
+		agricultor.completedAt = new Date().getTime();
+	} else {
+		agricultor.completed = false
+		agricultor.completedAt = null
+	}
+
+	Agricultor.findByIdAndUpdate(id, {$set: agricultor}, {new: true}).then((agricultorEdited) => {
+		if (!agricultorEdited) {
+			return res.status(404).send()
+		}
+
+		return res.send({agricultorEdited})
+	}).catch((e) => {
+		console.log(e);
+		return res.status(400).send()
+	});
+};
+
+const getList = (req, res) => {
+	Agricultor.find().then((agricultorList) => {
+		return res.send({agricultorList})
+	}), (e) => {
+		return res.status(400).send(e)
+	}
+};
+
+const getById = (req, res) => {
+	var id = req.params.id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	Agricultor.findById(id).then((agricultor) => {
+		if (!agricultor) {
+			return res.status(404).send()		
+		}
+		return res.send({agricultor})
+	}).catch((e) => res.status(400).send())
+};
+
+module.exports = {
+    create,
+	remove,
+	update,
+	getList,
+	getById
+};
--- routes/agricultor.js
+++ routes/agricultor.js
@@ -5,5 +5,9 @@ var agricultorController = require('../controllers/agricultor');
 var agricultorRouter = express.Router();
 
 agricultorRouter.route('/create').post(agricultorController.create);
+agricultorRouter.route('/remove/:id').delete(agricultorController.remove);
+agricultorRouter.route('/update').patch(agricultorController.update);
+agricultorRouter.route('/getById/:id').get(agricultorController.getById);
+agricultorRouter.route('/getList').get(agricultorController.getList);
 
 module.exports = { agricultorRouter };

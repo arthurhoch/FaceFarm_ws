9c26bee6755b3c977972575eb35b195869d4e964
arthurhoch <arthur.j.h96@gmail.com>
2017-08-21 13:31:22 -0300

    Ajuste nas pastas

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-require('./config/config');
+require('./server/config/config');
 
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const requireDir = require('require-dir');
 
-const { mongoose } = require('./db/mongoose');
+const { mongoose } = require('./server/db/mongoose');
 
-const { agricultorRouter } = require('./routes/agricultorRouter');
-const { anuncioRouter } = require('./routes/anuncioRouter');
-const { banimentoRouter } = require('./routes/banimentoRouter');
-const { cidadeRouter } = require('./routes/cidadeRouter');
-const { comentarioRouter } = require('./routes/comentarioRouter');
-const { configuracaoUsuarioRouter } = require('./routes/configuracaoUsuarioRouter');
-const { culturaRouter } = require('./routes/culturaRouter');
-const { denunciaAnuncioRouter } = require('./routes/denunciaAnuncioRouter');
-const { denunciaPostagemRouter } = require('./routes/denunciaPostagemRouter');
-const { empresaRouter } = require('./routes/empresaRouter');
-const { entropiaAnuncioRouter } = require('./routes/entropiaAnuncioRouter');
-const { estadoRouter } = require('./routes/estadoRouter');
-const { ferramentasAgricolasRouter } = require('./routes/ferramentasAgricolasRouter');
-const { gerenteRouter } = require('./routes/gerenteRouter');
-const { imagemRouter } = require('./routes/imagemRouter');
-const { moderadorRouter } = require('./routes/moderadorRouter');
-const { motivoRouter } = require('./routes/motivoRouter');
-const { notificacaoRouter } = require('./routes/notificacaoRouter');
-const { planoAnuncioRouter } = require('./routes/planoAnuncioRouter');
-const { postagemRouter } = require('./routes/postagemRouter');
-const { tipoPostagemRouter } = require('./routes/tipoPostagemRouter');
+const { agricultorRouter } = require('./server/routes/agricultorRouter');
+const { anuncioRouter } = require('./server/routes/anuncioRouter');
+const { banimentoRouter } = require('./server/routes/banimentoRouter');
+const { cidadeRouter } = require('./server/routes/cidadeRouter');
+const { comentarioRouter } = require('./server/routes/comentarioRouter');
+const { configuracaoUsuarioRouter } = require('./server/routes/configuracaoUsuarioRouter');
+const { culturaRouter } = require('./server/routes/culturaRouter');
+const { denunciaAnuncioRouter } = require('./server/routes/denunciaAnuncioRouter');
+const { denunciaPostagemRouter } = require('./server/routes/denunciaPostagemRouter');
+const { empresaRouter } = require('./server/routes/empresaRouter');
+const { entropiaAnuncioRouter } = require('./server/routes/entropiaAnuncioRouter');
+const { estadoRouter } = require('./server/routes/estadoRouter');
+const { ferramentasAgricolasRouter } = require('./server/routes/ferramentasAgricolasRouter');
+const { gerenteRouter } = require('./server/routes/gerenteRouter');
+const { imagemRouter } = require('./server/routes/imagemRouter');
+const { moderadorRouter } = require('./server/routes/moderadorRouter');
+const { motivoRouter } = require('./server/routes/motivoRouter');
+const { notificacaoRouter } = require('./server/routes/notificacaoRouter');
+const { planoAnuncioRouter } = require('./server/routes/planoAnuncioRouter');
+const { postagemRouter } = require('./server/routes/postagemRouter');
+const { tipoPostagemRouter } = require('./server/routes/tipoPostagemRouter');
 
 var app = express();
 app.use(bodyParser.json());
@@ -58,30 +58,3 @@ app.use('/tipoPostagem', tipoPostagemRouter);
 app.listen(process.env.PORT, () => {
     console.log(`Server listen on port ${process.env.PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

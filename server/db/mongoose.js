e80e27c081e056faea314abdf942578949f77870
Arthur Hoch <arthur.j.h96@gmail.com>
2017-08-22 19:03:20 -0300

    Fix warnings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+	keepAlive: true,
+	reconnectTries: Number.MAX_VALUE,
+	useMongoClient: true
+});
 
 console.log(process.env.MONGODB_URI);
 
